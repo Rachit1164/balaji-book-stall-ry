326e0377f21df7059b33eb61f62d3b80398c1917
Rachit1164 <rachitsolanki41@gmail.com>
2025-09-16 14:04:31 +0530

    Add Footer.js

---
+++ Root/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "10px", backgroundColor: "#f5f5f5", textAlign: "center" }}>
+      <p>© 2025 Balaji Book Stall</p>
+    </footer>
+  );
+}
